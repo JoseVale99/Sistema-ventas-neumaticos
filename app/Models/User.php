6c9e04aaa8d1ec0c15dfc1f72184e3e78b2631b2
ReyesHernandezYairHazael <YairGL@yandex.com>
2021-08-20 00:25:09 -0500

    api-crud cliente

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use App\Notifications\MyResetPassword;
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
     use HasRoles;
     /**
      * The attributes that are mass assignable.
@@ -56,17 +58,17 @@ class User extends Authenticatable
     //     return $this->hasMany('App\Article');
     // }
 
-         // función para la búsqueda de usuarios
-         public function scopeBuscar($query, $tipo, $buscar) {
-            if ( ($tipo) && ($buscar) ) {
-                return $query->where($tipo,'like',"%$buscar%");
-            }
-          
+    // función para la búsqueda de usuarios
+    public function scopeBuscar($query, $tipo, $buscar) {
+        if ( ($tipo) && ($buscar) ) {
+            return $query->where($tipo,'like',"%$buscar%");
         }
-        public function sendPasswordResetNotification($token)
-        {
-            $this->notify(new MyResetPassword($token));
-        }  
-        
+
+    }
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new MyResetPassword($token));
+    }
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -28,39 +28,39 @@ Route::get('/', function () {
 Route::group(['middleware' =>'auth'], function(){
 
     // Rutas Dashboard admin
- Route::resource('productos', ProductosController::class);
- Route::resource('permission', PermissionController::class)->only(['index','edit','update','destroy']);
- Route::resource('role', RolesController::class);
- Route::resource('user', UserController::class)->only(['index','edit','update']);
- 
- 
+    Route::resource('productos', ProductosController::class);
+    Route::resource('permission', PermissionController::class)->only(['index','edit','update','destroy']);
+    Route::resource('role', RolesController::class);
+    Route::resource('user', UserController::class)->only(['index','edit','update']);
+
+
 
 
 // pedidos
-Route::get('/Pedidos/index',[PedidoController::class,'index'])->name('pedido.index')->middleware('can:pedido.index'); 
-Route::delete('/Pedidos/borrar/{id}',[PedidoController::class,'destroy'])->name('pedido.destroy')->middleware('can:pedido.destroy'); 
+    Route::get('/Pedidos/index',[PedidoController::class,'index'])->name('pedido.index')->middleware('can:pedido.index');
+    Route::delete('/Pedidos/borrar/{id}',[PedidoController::class,'destroy'])->name('pedido.destroy')->middleware('can:pedido.destroy');
 
-//  Reportes 
-Route::get('/Reportes/index',[ReporteController::class,'index'])->name('reporte.index')->middleware('can:reporte.index');
-Route::get('/Reportes/pdf', [ReporteController::class, 'createPDF'])->name('reporte-pdf')->middleware('can:reporte-pdf');
+//  Reportes
+    Route::get('/Reportes/index',[ReporteController::class,'index'])->name('reporte.index')->middleware('can:reporte.index');
+    Route::get('/Reportes/pdf', [ReporteController::class, 'createPDF'])->name('reporte-pdf')->middleware('can:reporte-pdf');
 
 // ::::::::::::::::: cliente ::::::::::::::::::::::::::::::::
 
-// Rutas del carrito cliente 
-Route::post('/Cart/cart_add', [CarritoController::class,'add'])->name('cart.add');
+// Rutas del carrito cliente
+    Route::post('/Cart/cart_add', [CarritoController::class,'add'])->name('cart.add');
 // Mostrar el carrito
-Route::get('/Cart/cart-checkout',[CarritoController::class,'cart'])->name('cart.checkout')->middleware('can:cart.checkout');
+    Route::get('/Cart/cart-checkout',[CarritoController::class,'cart'])->name('cart.checkout')->middleware('can:cart.checkout');
 // Borrar todo el carrito
-Route::post('/Cart/cart-clear', [CarritoController::class,'clear'])->name('cart.clear');
+    Route::post('/Cart/cart-clear', [CarritoController::class,'clear'])->name('cart.clear');
 // Renover un intem de carrito
-Route::post('/Cart/cart-removeitem', [CarritoController::class,'removeitem'])->name('cart.removeitem');
+    Route::post('/Cart/cart-removeitem', [CarritoController::class,'removeitem'])->name('cart.removeitem');
 
 // Mueestra la vista principal del carrito
-Route::get('/Cart/Carrito', [CarritoController::class,'index'])->name('cart.cart')->middleware('can:cart.cart');
+    Route::get('/Cart/Carrito', [CarritoController::class,'index'])->name('cart.cart')->middleware('can:cart.cart');
 
 // Clientes CRUD--- -----
 
-Route::resource('clientes', ClienteController::class);
+    Route::resource('clientes', ClienteController::class);
 
 
 
@@ -71,18 +71,18 @@ Route::resource('clientes', ClienteController::class);
 
 
 // compra
-Route::get('/stripe', [CarritoController::class, 'stripe'])->name('cart.stripe');
-Route::post('/stripe', [CarritoController::class, 'stripePost'])->name('stripe.post');
+    Route::get('/stripe', [CarritoController::class, 'stripe'])->name('cart.stripe');
+    Route::post('/stripe', [CarritoController::class, 'stripePost'])->name('stripe.post');
 // Facturas
-Route::get('/Cart/invoice', [CarritoController::class, 'invoices'])->name('cart.invoices')->middleware('can:cart.invoices');
-Route::get('/Cart/invoice/{id}', [CarritoController::class, 'createPDF'])->name('factura-pdf');
+    Route::get('/Cart/invoice', [CarritoController::class, 'invoices'])->name('cart.invoices')->middleware('can:cart.invoices');
+    Route::get('/Cart/invoice/{id}', [CarritoController::class, 'createPDF'])->name('factura-pdf');
 
 
 // all users
 
-Route::get('/user/profile',[UserController::class,'profile'])->name('user.profile');
-Route::get('/user/profile/{id}',[UserController::class,'show'])->name('user.show');
-Route::put('/user/profile/update/{id}',[UserController::class,'userUpdate'])->name('user.editar');
+    Route::get('/user/profile',[UserController::class,'profile'])->name('user.profile');
+    Route::get('/user/profile/{id}',[UserController::class,'show'])->name('user.show');
+    Route::put('/user/profile/update/{id}',[UserController::class,'userUpdate'])->name('user.editar');
 
 });
 
