3a613419070dd89df69143050cce73a3eda8812b
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-08-24 21:41:19 -0500

    production

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,25 +10,25 @@
     <meta name="author" content="">
 
     <title>  @yield('titulo','main')  </title>
-    <link rel="shortcut icon" href="https://i.ibb.co/2YBhLXC/Michelin-Logo.png">
+    <link rel="shortcut icon" href="https://i.ibb.co/b6RqSDY/Logo-removebg-preview.png">
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
     <!-- Custom fonts for this template -->
     
     <link 
-    href="{!! asset('vendor/fontawesome-free/css/all.min.css')!!}" rel="stylesheet" type="text/css">
+    href="{!! secure_asset('vendor/fontawesome-free/css/all.min.css')!!}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="{!! asset('css/main.css') !!}">
+    <link rel="stylesheet" href="{!! secure_asset('css/main.css') !!}">
 
 </head>
 
@@ -36,33 +36,33 @@
 
     @yield('contenido') 
           <!-- Bootstrap core JavaScript-->
-          <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-          <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+          <script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+          <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
  
              
           <!-- Core plugin JavaScript-->
-          <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+          <script src="{{ secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
              
           <!-- Custom scripts for all pages-->
-          <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+          <script src="{{ secure_asset('js/sb-admin-2.min.js') }}"></script>
 
           <!-- Page level plugins -->
-          <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
-          <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+          <script src="{{ secure_asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+          <script src="{{ secure_asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
        
           <!-- Page level custom scripts -->
-          <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
-          <script src="{{ asset('js/main.js') }}"></script> 
+          <script src="{{ secure_asset('js/demo/datatables-demo.js') }}"></script>
+          <script src="{{ secure_asset('js/main.js') }}"></script> 
 
       
    
 
 
     <!-- Page level plugins -->
-    <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ secure_asset('vendor/chart.js/Chart.min.js') }}"></script>
 
     <!-- Page level custom scripts -->
-    <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ secure_asset('js/demo/chart-area-demo.js') }}"></script>
    
 </body>
 
